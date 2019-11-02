095f8e591f61dd76e3602cc36f52f5a78aa7c1fa
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-11-01 19:35:00 -0500

    navbar created

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,1 +1,47 @@
 // index.js for Navbar
+
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Home
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/auth"
+              className={
+                window.location.pathname === "/auth" ? "nav-link active" : "nav-link"}
+            >
+              Authenticate
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/parent"
+              className={window.location.pathname === "/parent" ? "nav-link active" : "nav-link"}
+            >
+              Parent
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/teacher"
+              className={window.location.pathname === "/teacher" ? "nav-link active" : "nav-link"}
+            >
+              Teacher
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
