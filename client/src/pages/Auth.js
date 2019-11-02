534582849cc9e873910f0a562af01d378e1cfd88
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-11-01 19:59:55 -0500

    added jumbotron compented to Auth

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,15 @@
 // display the home page that should contain the authentication
 import React from "react";
 
+import Jumbotron from "../components/Jumbotron";
+
 function Auth() {
     return (
 <div>
-    <p>This is the home page</p>
+<Jumbotron backgroundImage="">
+        <h1>This is the home page</h1>
+        <h2>Welcome with a login</h2>
+      </Jumbotron>
 </div>
     );
 
