168bafeaf47f6362a8832c59f334eb44e77c0521
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-11-01 19:37:21 -0500

    connection to file path made

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,1 +1,13 @@
 // display the home page that should contain the authentication
+import React from "react";
+
+function Auth() {
+    return (
+<div>
+    <p>This is the home page</p>
+</div>
+    );
+
+}
+
+export default Auth;
