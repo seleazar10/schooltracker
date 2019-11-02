63853fc3d5e060a9f1f6014849cebbebe1566660
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-11-01 19:34:37 -0500

    pathing for navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import logo from './logo.svg';
+import Navbar from "./components/Navbar";
+import Auth from "./pages/Auth";
+import Parent from "./pages/Parent";
+import Teacher from "./pages/Teacher";
+import Wrapper from "./components/Wrapper";
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Riko is the man!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Navbar />
+      <Wrapper>
+      <Route exact path="/" component={Auth} />
+          <Route exact path="/auth" component={Auth} />
+          <Route exact path="/parent" component={Parent} />
+          <Route exact path="/teacher" component={Teacher} />
+      </Wrapper>
     </div>
+    </Router>
   );
 }
 
