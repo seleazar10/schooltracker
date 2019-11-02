2c0cb3ed12a406fae150af408c5b30af6acf9dfa
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-11-01 19:37:33 -0500

    connection to file path made

---
+++ client/src/pages/Parent.js
@@ -0,0 +1,13 @@
+// display the parents page that should contain the parent info
+import React from "react";
+
+function Parent() {
+    return (
+<div>
+    <p>This is the parents page</p>
+</div>
+    );
+
+}
+
+export default Parent;
