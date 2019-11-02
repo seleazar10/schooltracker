f2e67593c6c5d643e5d2f9a07aac09ec587a1ee3
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-11-02 14:08:34 -0500

    updated boilerplate

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -6,13 +6,13 @@ import Jumbotron from "../components/Jumbotron";
 function Auth() {
     return (
 <div>
-<Jumbotron backgroundImage="">
+<Jumbotron backgroundImage="../../images/homepageExample.jpg">
         <h1>This is the home page</h1>
         <h2>Welcome with a login</h2>
       </Jumbotron>
 </div>
-    );
 
+    );
 }
 
 export default Auth;
