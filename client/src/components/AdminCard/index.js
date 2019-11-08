1a1a96e23b958fd0d14e7c9127ee2a76a4af4db7
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-11-07 21:21:14 -0600

    added a new card

--- client/src/components/AdminCard/index.js
+++ client/src/components/AdminCard/index.js
@@ -4,16 +4,30 @@ import React from "react";
 import "./style.css"
 
 
-export function Card(props) {
-    return <div className="card mt-5">
+export function CardAnnouncements(props) {
+  return <div className="card mt-5">
     <div className="card-body text-center">
       <h1 cclassName="card-title">Card title</h1>
       <h4 className="card-text">Announcements</h4>
     </div>
     <div>
-        {props.children}
+      {props.children}
     </div>
   </div>;
-  }
+}
 
-  export default Card;
+export function CardAnnModify(props) {
+  return <div className="card mt-5">
+    <div className="card-body text-center">
+      <h3 className="card-title">Modify Announcements</h3>
+      <h4>Enter the new announcement below:</h4>
+    </div>
+    <div>
+      {props.children}
+    </div>
+  </div>;
+}
+
+
+
+
