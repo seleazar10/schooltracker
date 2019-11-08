013ffe6d2c1181d183c79b927bc7c4136df51fa1
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-11-07 19:53:19 -0600

    changed the rows in Text area and added mt mb to Formbtn

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -15,14 +15,14 @@ export function Input(props) {
 export function TextArea(props) {
   return (
     <div className="form-group">
-      <textarea className="form-control" rows="20" {...props} />
+      <textarea className="form-control" rows="3" {...props} />
     </div>
   );
 }
 
 export function FormBtn(props) {
   return (
-    <div className="col-4">
+    <div className="col-4 mt-3 mb-4">
     <button {...props} className="btn btn-success">
       {props.children}
     </button>
