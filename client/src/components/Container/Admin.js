4fd26d40b9f30d004b2abb88d78d886d3c147e68
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-11-11 20:29:46 -0600

    created form Name and Username entries

--- client/src/components/Container/Admin.js
+++ client/src/components/Container/Admin.js
@@ -1,35 +1,56 @@
 import React from "react";
+import { Button, Form, Container } from 'react-bootstrap';
 
 
 class Box extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-      }
-    
-      render() {
-        return (
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Name:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-        );
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      teacherName: '',
+      userName: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ teacherName: event.target.value });
+  }
+
+  handleChangeUsername(event) {
+    this.setState({ userName: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log('A name was submitted: ' + this.state.teacherName + " " + this.state.userName);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <Container className="bg-light mb-3 p-2">
+        <Form onSubmit={this.handleSubmit}>
+
+          <Form.Group controlId="newTeacherName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter Name" value={this.state.teacherName} onChange={this.handleChange} />
+          </Form.Group>
+
+          <Form.Group controlId="newUserName">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="Enter Username" value={this.state.userName} onChange={this.handleChangeUsername} />
+          </Form.Group>
+
+          <Button variant="primary" type="submit" value="Submit">
+            Submit
+          </Button>
+
+        </Form>
+      </Container>
+    );
+  }
 }
 
 export default Box;
