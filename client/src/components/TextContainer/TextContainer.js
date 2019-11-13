caf4ada6571a99b3b6c322d71dbe1cba42ed54ec
Darrian Bethea <codingstudentdarrian@gmail.com>
2019-11-13 16:56:21 -0600

    new online text

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -6,16 +6,12 @@ import './TextContainer.css';
 
 const TextContainer = ({ users }) => (
     <div className="textContainer">
-        <div>
-            <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
-            <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
-            <h2>Try it out right now! <span role="img" aria-label="emoji">⬅️</span></h2>
-        </div>
+
         {
             users
                 ? (
                     <div>
-                        <h1>People currently chatting:</h1>
+                        <h1>Users Online &nbsp; &nbsp; &nbsp; </h1>
                         <div className="activeContainer">
                             <h2>
                                 {users.map(({ name }) => (
