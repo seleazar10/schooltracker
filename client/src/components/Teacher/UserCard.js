f9c2bf1eb8286bfa485934232e3524754cf11539
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-11-14 20:38:35 -0600

    format document

--- client/src/components/Teacher/UserCard.js
+++ client/src/components/Teacher/UserCard.js
@@ -1,20 +1,14 @@
 import React from "react";
 
-function UserCard(props){
-    return(
-        
-        // <div className="card usercard users col-2">
-        //     <input type="checkbox" checked={props.user.selected} onChange={(event)=> props.handleChange(props.user.id)} ></input>
-        //     <p>{props.user.name}</p>
-        // </div>
+function UserCard(props) {
+    return (
+
         <div className="card usercard users col-2">
-            <input type="checkbox" checked={props.user.selected} onChange={(event)=> props.handleChange(props.user.id)} ></input>
+            <input type="checkbox" checked={props.user.selected} onChange={(event) => props.handleChange(props.user._id)} ></input>
             <p>{props.user.name}</p>
         </div>
+
     )
 }
-
-
-
 
 export default UserCard;
