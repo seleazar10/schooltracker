fff351c3140f54e7f56b9593a339e9a34768e7ef
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-11-16 13:44:54 -0600

    changed file name Icons to icons

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import onlineIcon from '../../Icons/onlineIcon.png';
+import onlineIcon from '../../icons/onlineIcon.png';
 
 import './TextContainer.css';
+
 
 const TextContainer = ({ users }) => (
     <div className="textContainer">
